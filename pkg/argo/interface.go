c52676cb1c3df07e7404822d3d16a012d402df34
hanjunlee <opsdownn@gmail.com>
2020-01-19 20:45:37 +0900

    add use case

--- pkg/argo/interface.go
+++ pkg/argo/interface.go
@@ -7,6 +7,15 @@ import (
 	wf "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
+// UseCase is the interface of use case.
+type UseCase interface {
+	Get(key string) *wf.Workflow
+	Search(pattern string) []*wf.Workflow
+	Delete(key string) error
+	// Logs get the channel to recieve Logs from a Argo workflow.
+	Logs(ctx context.Context, key string) (ch <-chan Log, err error)
+}
+
 // Repository is the interface of repositories.
 type Repository interface {
 	Get(key string) *wf.Workflow
