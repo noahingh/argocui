a0773956577a72ca0b3668ba01a29293f843ea13
hanjunlee <opsdownn@gmail.com>
2020-01-24 13:22:50 +0900

    add the info view

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/jroimartin/gocui"
 
+	"github.com/hanjunlee/argocui/internal/app/views/info"
 	"github.com/hanjunlee/argocui/internal/app/views/list"
 	"github.com/hanjunlee/argocui/pkg/argo"
 )
@@ -24,15 +25,15 @@ func ManagerFunc(s argo.UseCase, g *gocui.Gui) func(g *gocui.Gui) error {
 
 		maxX, maxY := g.Size()
 
-		// err = layoutInfo(g, 1, 0, maxX/5-1, maxY/4-1)
-		// if err != nil {
-		// 	return err
-		// }
+		err = info.LayoutInfo(g, 1, 0, maxX/5-1, maxY/4-1)
+		if err != nil {
+			return err
+		}
 
-		// err = layoutBrand(g, maxX/5, 0, maxX-1, maxY/4-1)
-		// if err != nil {
-		// 	return err
-		// }
+		err = info.LayoutBrand(g, maxX/5, 0, maxX-1, maxY/4-1)
+		if err != nil {
+			return err
+		}
 
 		// err = layoutSearch(g, 0, maxY/4-2, maxX-1, maxY/4)
 		// if err != nil {
---
+++ internal/app/views/info/layout.go
@@ -0,0 +1,93 @@
+package info
+
+import (
+	"fmt"
+	"time"
+
+	argoutil "github.com/hanjunlee/argocui/pkg/util/argo"
+	"github.com/hanjunlee/argocui/pkg/util/color"
+	viewutil "github.com/hanjunlee/argocui/pkg/util/view"
+	"github.com/jroimartin/gocui"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	infoViewName  = "info"
+	brandViewName = "brand"
+)
+
+var (
+	log = logrus.WithFields(logrus.Fields{
+		"pkg":  "info",
+		"file": "layout.go",
+	})
+)
+
+// LayoutInfo lay out the view of informations of Argo CUI.
+func LayoutInfo(g *gocui.Gui, x0, y0, x1, y1 int) error {
+	v, err := g.SetView(infoViewName, x0, y0, x1, y1)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Frame = false
+
+		go viewutil.RefreshViewPeriodic(g, v, 1*time.Second, func() error {
+			const (
+				argoRev    = "v2.4.1"
+				argocuiRev = "v0.0.1"
+				homePage   = "github.com/hanjunlee/argocui"
+			)
+			var (
+				context   string
+				namespace string
+				err       error
+			)
+
+			if context, err = argoutil.GetCurrentContext(); err != nil {
+				log.Errorf("couldn't get the context: %s.", err)
+				context = ""
+			}
+
+			if namespace, err = argoutil.GetNamespace(); err != nil {
+				log.Errorf("couldn't get the namespace: %s.", err)
+				namespace = ""
+			}
+
+			v.Clear()
+			fmt.Fprintln(v, "")
+			fmt.Fprintf(v, "Context:     %s\n", color.ChangeColor(context, gocui.ColorYellow))
+			fmt.Fprintf(v, "Namespace:   %s\n", color.ChangeColor(namespace, gocui.ColorYellow))
+			fmt.Fprintf(v, "Argo Rev:    %s\n", color.ChangeColor(argoRev, gocui.ColorYellow))
+			fmt.Fprintf(v, "Argocui Rev: %s\n", color.ChangeColor(argocuiRev, gocui.ColorYellow))
+			fmt.Fprintf(v, "Homepage:    %s\n", color.ChangeColor(homePage, gocui.ColorYellow))
+			return nil
+		})
+	}
+
+	return nil
+}
+
+// LayoutBrand lay out the view of brand.
+func LayoutBrand(g *gocui.Gui, x0, y0, x1, y1 int) error {
+	v, err := g.SetView(brandViewName, x0, y0, x1, y1)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Frame = false
+		brand := `
+     _____                            _________       .__ 
+    /  _  \_______  ____   ____       \_   ___ \ __ __|__|
+   /  /_\  \_  __ \/ ___\ /  _ \ --   /    \  \/|  |  \  |
+  /    |    \  | \/ /_/  >  <_> ) --- \     \___|  |  /  |
+  \____|__  /__|  \___  / \____/ --    \______  /____/|__|
+       	  \/     /_____/                      \/          
+`
+		fmt.Fprintf(v, color.ChangeColor(brand, gocui.ColorYellow))
+	}
+
+	return nil
+}
