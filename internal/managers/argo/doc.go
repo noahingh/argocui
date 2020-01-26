f01e8914c4b270ba3369d56829d626b7c802ddc3
hanjunlee <opsdownn@gmail.com>
2020-01-26 11:17:28 +0900

    add doc in managers/argo

---
+++ internal/managers/argo/doc.go
@@ -0,0 +1,8 @@
+/*
+
+Package argo is the manager of views which is relevant to the Argo workflow,
+and it is consist of collection(same as `argo list`), sub, follower(`argo logs`) and tree(`argo get`).
+Each component, generally, has layout, keybinding and subscribe method for the view.
+
+*/
+package argo
